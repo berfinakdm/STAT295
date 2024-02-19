34cb97c3698e0067438f2310f0dd6db1e7958c62
berfinakdm <akdemir.berfin@metu.edu.tr>
2024-02-19 15:58:03 +0300

    first class

--- 295(1).R
+++ 295(1).R
@@ -47,8 +47,44 @@ usethis::use_git_config(
   user.email = "akdemir.berfin@metu.edu.tr"
 )
 
+usethis::create_github_token()
+gitcreds::gitcreds_set()
 
+data(iris)
+head(iris)
+class(iris)
+class(iris$Sepal.Length)
+str(iris)
+summary(iris)
 
+iris$Sepal.Length
+iris[1,]
+iris[,1]
+iris[, "Species"]
+
+table(iris$Species)
+LogicIndex <- iris[, "Petal.Length"] > 5.5
+iris[LogicIndex ,]
+mean(iris$Sepal.Length)
+sd(iris$Sepal.Width)
+
+#functions
+
+MeanAndStd <- function(x) {
+  c(mean = mean(x), std = sd(x))
+}
+MeanAndStd(iris$Sepal.Length)
+
+Stats <- aggregate(Sepal.Length ~ Species, data = iris, 
+                   FUN = MeanAndStd)
+Stats
+
+#visualize 
+
+plot(iris$Sepal.Length, col = as.numeric(iris$Species), ylab = "Sepal Length" )
+legend('topleft', legend = levels(iris$Species), pch = 1:3)
+
+boxplot(Sepal.Length ~ Species, data = iris)
 
 
 
